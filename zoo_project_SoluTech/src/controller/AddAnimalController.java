a3b8643bfdd477e1b8b88c048b5f1f95c425173f
uji100 <97239721+uji100@users.noreply.github.com>
2023-02-02 14:11:39 +0530

    AddAnimalController attributes setup.

--- zoo_project_SoluTech/src/controller/AddAnimalController.java
+++ zoo_project_SoluTech/src/controller/AddAnimalController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class AddAnimalController {
+    public  TextField animalId;
+    public TextField animalName;
+    public TextField animalAge;
+    public DatePicker animalBornDate;
+    public DatePicker animalDethDate;
+
 }
